536e17cffde0107cb74e348c8f15471d84c56e44
Lucas Moyer <lucasdmoyer@gmail.com>
2019-08-25 11:15:02 -0500

    added model prediction

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 from flask import Flask, redirect, url_for, request, render_template
-from tensorflow.python.keras.applications import ResNet50
-from PIL import Image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+import sys
+import os
+import glob
+import re
 import numpy as np
-
+from tensorflow.keras.applications.resnet50 import ResNet50
+from werkzeug.utils import secure_filename
 app = Flask(__name__, template_folder='templates')
 
 MODEL_PATH = 'models/your_model.h5'
@@ -15,16 +21,36 @@ def index():
 	return render_template('index.html')
 
 def model_predict(img_path, model):
-	img = Image.open(img_path)
-	x = np.array(img)
+	img = image.load_img(img_path, target_size=(224, 224))
+	# Preprocessing the image
+	x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+	x = np.expand_dims(x, axis=0)
 	preds = model.predict(x)
 	return preds
 
-@app.route('/predict')
+@app.route('/predict', methods=['GET', 'POST'])
 def upload():
-	if request.method == 'POST':
-		f = request.files['file']
-		f.save(file_path)
-		preds = model_predict(file__path, model)
-		pred_class = decode_predictions(preds, top=1)
-		return pred_class
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['image']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run()
